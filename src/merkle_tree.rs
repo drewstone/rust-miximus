1f00fcc5619dcfcae5c2c809c9f846c7fa045d9a
Drew Stone <drewstone329@gmail.com>
2019-02-13 18:50:20 +0200

    Add merkle construction out of circuit

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -1,62 +1,101 @@
-use MerkleTreeCircuit;
-use std::error::Error;
-
-
-use num_bigint::BigInt;
-use num_traits::Num;
-
-use rand::{ChaChaRng, SeedableRng};
-use ff::{BitIterator, PrimeField, PrimeFieldRepr, Field};
+use ff::{BitIterator, PrimeField, Field};
 use pairing::{bn256::{Bn256, Fr}};
 use sapling_crypto::{
     babyjubjub::{
-        fs::Fs,
         JubjubBn256,
-        FixedGenerators,
-        JubjubEngine,
-        JubjubParams,
-        edwards::Point
     },
-    circuit::{
-        baby_ecc::fixed_base_multiplication,
-        boolean::{self, AllocatedBit, Boolean},
-        num
-    }
 };
 
+use tree::Tree;
 
-
-pub fn build_merkle_tree(mut leaves: Vec<pairing::bn256::Fr>, depth: usize) -> pairing::bn256::Fr {
-	if leaves.len() == 2 {
-		return hash_leaf_pair(depth, leaves[0], leaves[1]);
-	}
-	while (2 << depth) - leaves.len() > 0 {
-		leaves.push(pairing::bn256::Fr::zero());
-	}
-
-	let mut new_leaves: Vec<pairing::bn256::Fr> = vec![];
-    for i in 0..leaves.len() {
-    	if i % 2 != 0 { continue }
-        let lhs = leaves[i];
-        let rhs = leaves[i+1];
-        let cur = hash_leaf_pair(i, lhs, rhs);
-        new_leaves.push(cur);
-    }
-
-	return build_merkle_tree(new_leaves, depth - 1);
+#[derive(Debug)]
+pub struct MerkleTree<T> {
+    root: Tree<T>,
 }
 
-fn hash_leaf_pair(index: usize, lhs: pairing::bn256::Fr, rhs: pairing::bn256::Fr) -> pairing::bn256::Fr {
-	let params = &JubjubBn256::new();
-    let mut lhs: Vec<bool> = BitIterator::new(lhs.into_repr()).collect();
-    let mut rhs: Vec<bool> = BitIterator::new(rhs.into_repr()).collect();
+pub fn create_leaf_from_preimage<T>(nullifier: pairing::bn256::Fr, secret: pairing::bn256::Fr) -> Tree<T> {
+    let params = &JubjubBn256::new();
+    let mut lhs: Vec<bool> = BitIterator::new(nullifier.into_repr()).collect();
+    let mut rhs: Vec<bool> = BitIterator::new(secret.into_repr()).collect();
     lhs.reverse();
     rhs.reverse();
-	sapling_crypto::baby_pedersen_hash::pedersen_hash::<Bn256, _>(
-        sapling_crypto::baby_pedersen_hash::Personalization::MerkleTree(index),
+    let hash = sapling_crypto::baby_pedersen_hash::pedersen_hash::<Bn256, _>(
+        sapling_crypto::baby_pedersen_hash::Personalization::NoteCommitment,
         lhs.into_iter()
            .take(Fr::NUM_BITS as usize)
            .chain(rhs.into_iter().take(Fr::NUM_BITS as usize)),
         params
-    ).into_xy().0
+    ).into_xy().0;
+    return Tree::Empty {
+        hash: hash,
+    };
+}
+
+pub fn build_merkle_tree<T>(mut leaves: Vec<Tree<T>>, depth: usize) -> MerkleTree<T> {
+    if leaves.len() == 2 {
+        let left = leaves.remove(0);
+        let right = leaves.remove(0);
+        return MerkleTree {
+            root: hash_leaf_pair(depth, left, right),
+        };
+    }
+    while (2 << depth) - leaves.len() > 0 {
+        leaves.push(Tree::Empty {
+            hash: <pairing::bn256::Fr>::zero(),
+        });
+    }
+
+    let mut new_leaves: Vec<Tree<T>> = vec![];
+    for i in 0..leaves.len() {
+        if i % 2 != 0 { continue }
+        let left = leaves.remove(0);
+        let right = leaves.remove(0);
+        let cur = hash_leaf_pair(i, left, right);
+        new_leaves.push(cur);
+    }
+
+    return build_merkle_tree(new_leaves, depth - 1);
+}
+
+fn hash_leaf_pair<T>(index: usize, lhs: Tree<T>, rhs: Tree<T>) -> Tree<T> {
+    let params = &JubjubBn256::new();
+    let mut lhs_bool: Vec<bool> = BitIterator::new(lhs.hash().into_repr()).collect();
+    let mut rhs_bool: Vec<bool> = BitIterator::new(rhs.hash().into_repr()).collect();
+    lhs_bool.reverse();
+    rhs_bool.reverse();
+    let hash = sapling_crypto::baby_pedersen_hash::pedersen_hash::<Bn256, _>(
+        sapling_crypto::baby_pedersen_hash::Personalization::MerkleTree(index),
+        lhs_bool.into_iter()
+           .take(Fr::NUM_BITS as usize)
+           .chain(rhs_bool.into_iter().take(Fr::NUM_BITS as usize)),
+        params
+    ).into_xy().0;
+    return Tree::Node {
+        hash: hash,
+        left: Box::new(lhs),
+        right: Box::new(rhs),
+    }
+}
+
+fn print_merkle_tree<T>(tree: &Tree<T>) {
+    println!("{:?}", tree.hash());
+    match tree {
+        Tree::Node { hash: _, ref left, ref right } => {
+            print_merkle_tree(left);
+            print_merkle_tree(right);   
+        },
+        _ => { return },
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use merkle_tree::print_merkle_tree;
+    use tree::Tree;
+    use super::build_merkle_tree;
+    #[test]
+    fn test_merkle_tree() {
+        let r: Tree<u32> = build_merkle_tree(vec![], 3).root;
+        print_merkle_tree(&r)
+    }
 }
---
+++ src/tree.rs
@@ -0,0 +1,160 @@
+/// Binary Tree where leaves hold a stand-alone value.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Tree<T> {
+    Empty {
+        hash: pairing::bn256::Fr,
+    },
+
+    Leaf {
+        hash: pairing::bn256::Fr,
+        value: T,
+    },
+
+    Node {
+        hash: pairing::bn256::Fr,
+        left: Box<Tree<T>>,
+        right: Box<Tree<T>>,
+    },
+}
+
+impl<T> Tree<T> {
+    /// Returns a hash from the tree.
+    pub fn hash(&self) -> &pairing::bn256::Fr {
+        match *self {
+            Tree::Empty { ref hash } => hash,
+            Tree::Leaf { ref hash, .. } => hash,
+            Tree::Node { ref hash, .. } => hash,
+        }
+    }
+
+    /// Returns a borrowing iterator over the leaves of the tree.
+    pub fn iter(&self) -> LeavesIterator<T> {
+        LeavesIterator::new(self)
+    }
+}
+/// An borrowing iterator over the leaves of a `Tree`.
+/// Adapted from http://codereview.stackexchange.com/q/110283.
+#[allow(missing_debug_implementations)]
+pub struct LeavesIterator<'a, T>
+where
+    T: 'a,
+{
+    current_value: Option<&'a T>,
+    right_nodes: Vec<&'a Tree<T>>,
+}
+
+impl<'a, T> LeavesIterator<'a, T> {
+    fn new(root: &'a Tree<T>) -> Self {
+        let mut iter = LeavesIterator {
+            current_value: None,
+            right_nodes: Vec::new(),
+        };
+
+        iter.add_left(root);
+
+        iter
+    }
+
+    fn add_left(&mut self, mut tree: &'a Tree<T>) {
+        loop {
+            match *tree {
+                Tree::Empty { .. } => {
+                    self.current_value = None;
+                    break;
+                }
+
+                Tree::Node {
+                    ref left,
+                    ref right,
+                    ..
+                } => {
+                    self.right_nodes.push(right);
+                    tree = left;
+                }
+
+                Tree::Leaf { ref value, .. } => {
+                    self.current_value = Some(value);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+impl<'a, T> Iterator for LeavesIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<&'a T> {
+        let result = self.current_value.take();
+
+        if let Some(rest) = self.right_nodes.pop() {
+            self.add_left(rest);
+        }
+
+        result
+    }
+}
+
+/// An iterator over the leaves of a `Tree`.
+#[allow(missing_debug_implementations)]
+pub struct LeavesIntoIterator<T> {
+    current_value: Option<T>,
+    right_nodes: Vec<Tree<T>>,
+}
+
+impl<T> LeavesIntoIterator<T> {
+    fn new(root: Tree<T>) -> Self {
+        let mut iter = LeavesIntoIterator {
+            current_value: None,
+            right_nodes: Vec::new(),
+        };
+
+        iter.add_left(root);
+
+        iter
+    }
+
+    fn add_left(&mut self, mut tree: Tree<T>) {
+        loop {
+            match tree {
+                Tree::Empty { .. } => {
+                    self.current_value = None;
+                    break;
+                }
+
+                Tree::Node { left, right, .. } => {
+                    self.right_nodes.push(*right);
+                    tree = *left;
+                }
+
+                Tree::Leaf { value, .. } => {
+                    self.current_value = Some(value);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+impl<T> Iterator for LeavesIntoIterator<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        let result = self.current_value.take();
+
+        if let Some(rest) = self.right_nodes.pop() {
+            self.add_left(rest);
+        }
+
+        result
+    }
+}
+
+impl<T> IntoIterator for Tree<T> {
+    type Item = T;
+    type IntoIter = LeavesIntoIterator<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        LeavesIntoIterator::new(self)
+    }
+}
